4479fcb3a48b44041b103e4f9388d816bae86d9b
Ananda Ravi Kuntadi <90196737+ARKNravi@users.noreply.github.com>
2023-03-11 00:17:11 +0700

    Add files via upload

--- initializer/connectToDb.go
+++ initializer/connectToDb.go
@@ -1,6 +1,8 @@
 package initializer
 
 import (
+	"ProjectBuahIn/buah"
+	"ProjectBuahIn/models"
 	"os"
 
 	"gorm.io/driver/mysql"
@@ -15,6 +17,12 @@ func ConnectToDb() {
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		panic("Failed to connect to DB")
+		panic("Failed to connect to DB 2345")
 	}
+	DB.AutoMigrate(&models.User{})
+	DB.AutoMigrate(&buah.Order{})
+	DB.AutoMigrate(&buah.Buah{})
+	DB.AutoMigrate(&buah.Cart{})
+	DB.AutoMigrate(&buah.Order{})
+
 }
