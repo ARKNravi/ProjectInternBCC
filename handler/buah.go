495026b51a4bfbeba11d4b421dbe05652e5f839d
Ananda Ravi Kuntadi <90196737+ARKNravi@users.noreply.github.com>
2023-03-11 00:16:54 +0700

    Add files via upload

--- handler/buah.go
+++ handler/buah.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"ProjectBuahIn/buah"
+	"ProjectBuahIn/models"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
@@ -17,6 +18,21 @@ type buahHandler struct {
 
 func NewBuahHandler(buahService buah.Service) *buahHandler {
 	return &buahHandler{buahService}
+}
+
+type OrderHandler interface {
+	OrderProduct(*gin.Context)
+}
+
+type orderHandler struct {
+	repo buah.OrderRepository
+}
+
+// NewOrderHandler --> return new Order Handler
+func NewOrderHandler() OrderHandler {
+	return &orderHandler{
+		repo: buah.NewOrderRepository(),
+	}
 }
 
 func (h *buahHandler) GetBuahs(c *gin.Context) {
@@ -41,20 +57,47 @@ func (h *buahHandler) GetBuahs(c *gin.Context) {
 	})
 }
 
-
-func (h *buahHandler) QueryHandler(c *gin.Context) {
-	title := c.Query("title")
-	price := c.Query("price")
-
-	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
-}
-
-func (h *buahHandler) GetBuah(c *gin.Context) {
+func (h *buahHandler) GetUser(c *gin.Context) {
 	idString := c.Param("id")
 
 	id, _ := strconv.Atoi(idString)
 
+	users, err := h.buahService.FindByIDuser(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": err,
+		})
+		return
+	}
+	user := convertToUserResponse(users)
+	c.JSON(http.StatusOK, gin.H{
+		"data": user,
+	})
+}
+
+func (h *buahHandler) GetBuah(c *gin.Context) {
+	idString := c.Param("nama")
+
+	id, _ := strconv.Atoi(idString)
+
 	b, err := h.buahService.FindByID(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": err,
+		})
+		return
+	}
+
+	buahResponse := convertToBuahResponse(b)
+	c.JSON(http.StatusOK, gin.H{
+		"data": buahResponse,
+	})
+
+}
+func (h *buahHandler) GetNamaBuah(c *gin.Context) {
+	idString := c.Query("nama")
+
+	b, err := h.buahService.FindByNama(idString)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"errors": err,
@@ -150,13 +193,51 @@ func (h *buahHandler) DeleteBuah(c *gin.Context) {
 	})
 }
 
+func (h *buahHandler) OrderBuah(c *gin.Context) {
+
+}
+
 func convertToBuahResponse(b buah.Buah) buah.BuahResponse {
 	return buah.BuahResponse{
-		ID:          b.ID,
-		Jenis:       b.Jenis,
+		Nama:        b.Nama,
 		Price:       b.Price,
 		Description: b.Description,
-		Matang:      b.Matang,
+		Quantity:    b.Quantity,
 		Discount:    b.Discount,
 	}
+}
+
+func convertToUserResponse(b models.User) models.User {
+	return models.User{
+		UserID:   b.UserID,
+		Username: b.Username,
+		Email:    b.Email,
+		Password: b.Password,
+	}
+}
+
+func (h *orderHandler) OrderProduct(ctx *gin.Context) {
+	prodIDStr := ctx.Query("product")
+	if prodID, err := strconv.Atoi(prodIDStr); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	} else {
+		quantityIDStr := ctx.Param("quantity")
+		if quantityID, err := strconv.Atoi(quantityIDStr); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			userID := ctx.GetFloat64("userID")
+			if err := h.repo.OrderProduct(int(userID), prodID, quantityID); err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{
+					"error": err.Error(),
+				})
+			} else {
+				ctx.String(http.StatusOK, "Product Successfully ordered")
+			}
+		}
+	}
+
 }
